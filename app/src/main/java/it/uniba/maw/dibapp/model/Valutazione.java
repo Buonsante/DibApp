5182b56422d2e57259a54530c208d40eee20cca9
Alessandro Santoro <ale.santoro@libero.it>
2019-08-21 18:40:34 +0200

    classe valutazione - Jimmy

--- app/src/main/java/it/uniba/maw/dibapp/model/Valutazione.java
+++ app/src/main/java/it/uniba/maw/dibapp/model/Valutazione.java
@@ -1,4 +1,18 @@
 package it.uniba.maw.dibapp.model;
 
+import java.time.LocalTime;
+import java.util.Date;
+
 public class Valutazione {
+
+    int id, voto, lezione;
+    String commento;
+    boolean visibilità;
+    Date data;
+    LocalTime ora;
+
 }
+
+
+
+
