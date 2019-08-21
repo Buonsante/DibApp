b7485486638f03d278e433cbaeb85e9512d86dd7
Vitangelo Buonsante <buonsantev@gmail.com>
2019-08-22 00:16:03 +0200

    Aggiunta navigation bar nella MainActivity. Aggiunti due fragment.

--- app/src/main/java/it/uniba/maw/dibapp/MainActivity.java
+++ app/src/main/java/it/uniba/maw/dibapp/MainActivity.java
@@ -1,13 +1,80 @@
 package it.uniba.maw.dibapp;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.TransitionRes;
+import android.support.design.widget.BottomNavigationView;
+import android.support.transition.Fade;
+import android.support.transition.Transition;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import it.uniba.maw.dibapp.fragment.CalendarFragment;
+import it.uniba.maw.dibapp.fragment.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // load the store fragment by default
+        toolbar.setTitle("Shop");
+        loadFragment(new CalendarFragment());
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.calendar:
+                    toolbar.setTitle("Calendar");
+                    fragment = new CalendarFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_gifts:
+                    toolbar.setTitle("My Gifts");
+                    fragment = new Fragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_cart:
+                    toolbar.setTitle("Cart");
+                    fragment = new Fragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.settings:
+                    toolbar.setTitle("Settings");
+                    fragment = new SettingsFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 }
