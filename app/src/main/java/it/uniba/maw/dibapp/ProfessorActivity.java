32ee7d38bce991d21d4f98f4e376a474ad5739b2
Marcello D'Agostino <marcello.d.agostino1997@hotmail.com>
2019-08-27 17:18:44 +0200

    Activity Professore

---
+++ app/src/main/java/it/uniba/maw/dibapp/ProfessorActivity.java
@@ -0,0 +1,89 @@
+package it.uniba.maw.dibapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import it.uniba.maw.dibapp.fragment.CalendarFragment;
+import it.uniba.maw.dibapp.fragment.SettingsFragment;
+import it.uniba.maw.dibapp.model.Lezione;
+
+public class ProfessorActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+    public List<Lezione> lezioni;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        Button btnHome=(Button)findViewById(R.id.Button1);
+        btnHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                // definisco l'intenzione
+                Intent openLesson = new Intent(ProfessorActivity.this,LessonActivity.class);
+                // passo all'attivazione dell'activity Lesson.java
+                startActivity(openLesson);
+            }
+        });
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // load the store fragment by default
+        toolbar.setTitle("Shop");
+        loadFragment(new SettingsFragment());
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.calendar:
+                    toolbar.setTitle("Calendar");
+                    fragment = new CalendarFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_gifts:
+                    toolbar.setTitle("My Gifts");
+                    fragment = new Fragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.settings:
+                    toolbar.setTitle("Settings");
+                    fragment = new SettingsFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        //transaction.addToBackStack("fragmentStack");
+        transaction.commit();
+    }
+
+
+}
