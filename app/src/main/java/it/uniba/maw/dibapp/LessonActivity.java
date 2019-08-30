fb74d6b84d18e80ff5a5dc431f9847939cecb9f7
Furio <valerio.furio92@gmail.com>
2019-08-30 18:16:31 +0200

    Creato fragment e interfaccia provvisoria bottom sheet dialog per la valutazione di una lezione

--- app/src/main/java/it/uniba/maw/dibapp/LessonActivity.java
+++ app/src/main/java/it/uniba/maw/dibapp/LessonActivity.java
@@ -3,6 +3,8 @@ package it.uniba.maw.dibapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
+
+import it.uniba.maw.dibapp.fragment.BottomSheetFragment;
 import it.uniba.maw.dibapp.fragment.CommentiFragment;
 import it.uniba.maw.dibapp.fragment.DettagliFragment;
 
@@ -10,8 +12,11 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.tabs.TabLayout;
 
 public class LessonActivity extends AppCompatActivity {
@@ -22,11 +27,22 @@ public class LessonActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
 
+    private Button buttonDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
         Log.d(TAG, "onCreate: Starting");
+
+        buttonDialog = findViewById(R.id.btn_bottom_sheet);
+
+        buttonDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showBottomSheetDialog();
+            }
+        });
 
 
         mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
@@ -49,5 +65,17 @@ public class LessonActivity extends AppCompatActivity {
         adapter.addFragment(dettagliFragment, "DETTAGLI");
         adapter.addFragment(new CommentiFragment(), "COMMENTI");
         viewPager.setAdapter(adapter);
+    }
+
+    private void showBottomSheetDialog() {
+//        // using BottomSheetDialog
+//        View dialogView = getLayoutInflater().inflate(R.layout.fragment_bottom_sheet, null);
+//        BottomSheetDialog dialog = new BottomSheetDialog(this);
+//        dialog.setContentView(dialogView);
+//        dialog.show();
+
+        // using BottomSheetDialogFragment
+        BottomSheetFragment bottomSheetFragment = new BottomSheetFragment();
+        bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
     }
 }
---
+++ app/src/main/java/it/uniba/maw/dibapp/fragment/BottomSheetFragment.java
@@ -0,0 +1,35 @@
+package it.uniba.maw.dibapp.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import it.uniba.maw.dibapp.R;
+
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+
+    public BottomSheetFragment() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
+    }
+}
