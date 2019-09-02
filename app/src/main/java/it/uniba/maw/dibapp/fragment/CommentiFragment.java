90863b41d233b66187199c351a4394e2dd232349
Vitangelo Buonsante <buonsantev@gmail.com>
2019-09-02 21:17:12 +0200

    Aggiunta visualizzazione realtime dei commenti

--- app/src/main/java/it/uniba/maw/dibapp/fragment/CommentiFragment.java
+++ app/src/main/java/it/uniba/maw/dibapp/fragment/CommentiFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,8 +15,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -75,14 +80,33 @@ public class CommentiFragment extends Fragment {
         // commenti prova
         Log.w(DEBUG_TAG, "caricamento commenti");
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection(lezione.getLinkLezione()+"/commenti").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-            @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                for(DocumentSnapshot d : queryDocumentSnapshots.getDocuments()) {
-                    Log.w(DEBUG_TAG, d.toString());
-                    valutazioni.add(new Valutazione(Float.parseFloat(d.getString("rating")), 0, d.getString("commento"), true, null, null));
+//        db.collection(lezione.getLinkLezione()+"/commenti").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+//            @Override
+//            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+//                for(DocumentSnapshot d : queryDocumentSnapshots.getDocuments()) {
+//                    Log.w(DEBUG_TAG, d.toString());
+//                    valutazioni.add(new Valutazione(Float.parseFloat(d.getString("rating")), 0, d.getString("commento"), true, null, null));
+//
+//                }
+//                mAdapter.notifyDataSetChanged();
+//            }
+//        });
 
+        db.collection(lezione.getLinkLezione()+"/commenti").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot snapshots, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(DEBUG_TAG, "Listen failed.", e);
+                    return;
                 }
+                for (DocumentChange d : snapshots.getDocumentChanges()) {
+                    Log.w(DEBUG_TAG, d.toString());
+                    if (d.getType() == DocumentChange.Type.ADDED) {
+                        Valutazione v = new Valutazione(Float.parseFloat(d.getDocument().getString("rating")), 0, d.getDocument().getString("commento"), true, null, null);
+                        valutazioni.add(v);
+                    }
+                }
+
                 mAdapter.notifyDataSetChanged();
             }
         });
